363330596b8cb6afde752d5bf7d2a78036523ae8
黄定坤 <baker211sh@gmail.com>
2019-01-08 14:38:08 +0800

    add Component Portfolio

---
+++ src/PortfolioUnit.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+import './PortfolioUnit.css';
+
+class PortfolioUnit extends Component {
+    // eslint-disable-next-line no-useless-constructor
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div>
+            </div>
+        );
+    }
+}
+
+ export default PortfolioUnit;
